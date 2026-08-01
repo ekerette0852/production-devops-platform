13ff5e96af075056ddcbfad9a453ef7286517722
Ekerette0852 <slylaw4u@gmail.com>
2026-08-01 22:43:49 +0000

    Remove public SSH access from web security group

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -27,18 +27,6 @@ resource "aws_security_group" "web" {
     ]
   }
 
-  ingress {
-    description = "SSH"
-
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-  }
-
   egress {
     from_port = 0
     to_port   = 0
