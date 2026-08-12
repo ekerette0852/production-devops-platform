d5bda37de2a8b09b37731a32d33f78f7ad900cbc
Ekerette0852 <slylaw4u@gmail.com>
2026-08-12 00:00:05 +0000

    feat: add CPU target tracking autoscaling policy

---
+++ terraform/modules/autoscaling/scaling.tf
@@ -0,0 +1,13 @@
+resource "aws_autoscaling_policy" "cpu_target_tracking" {
+  name                   = "${var.project_name}-${var.environment}-cpu-target-tracking"
+  autoscaling_group_name = aws_autoscaling_group.this.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
